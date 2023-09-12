72aeb35797b606455eaec2a8baba1a15f3ff2d6e
Denis Ivanov <d.ivanov@iac.spb.ru>
2023-09-12 12:59:59 +0400

    VonderanShop

--- src/basic.js
+++ src/basic.js
@@ -70,39 +70,64 @@ document.addEventListener('DOMContentLoaded', async () => {
         `
         dqs('#' + row.category).append(tr);
         counter++;
-        continue;
-        // let row = data.items[iKey];
-        // let rowItem = document.createElement('span');
-        //
-        // rowItem.innerText = row.name;
-        //
-        // let divRow = document.createElement('div');
-        // divRow.classList.add('row');
-        // divRow.classList.add('border');
-        // divRow.classList.add('border-success');
-        // divRow.classList.add('bg-secondary');
-        // divRow.classList.add('text-black');
-        // divRow.style.width = '400px';
-        // let cellSell = document.createElement('div');
-        // cellSell.classList.add('col-5');
-        // let cellBuy = document.createElement('div');
-        // cellBuy.classList.add('col-5');
-        // let cellName = document.createElement('div');
-        // cellName.classList.add('col-2');
-        // cellName.append(rowItem);
-        //
-        //
-        // let isSell = (row.isSell) ? 'text-success' : 'text-dunger';
-        // cellSell.innerHTML = "<span>Продажа: <span><span class='"+isSell+"'>" + row.price + "</span>";
-        //
-        // let isBuy = (row.isBuy) ? 'text-success' : 'text-dunger';
-        // cellBuy.innerHTML = "<span>Покупка: <span><span class='"+isBuy+"'>" + row.buy + "</span>";
-        //
-        // divRow.append(cellName);
-        // divRow.append(cellSell);
-        // divRow.append(cellBuy);
-        //
-        // //dqs('#' + row.category).append(rowItem);
-        // dqs('#' + row.category).append(divRow);
+    }
+
+    data = await fetch('./data/VonderanShop.json');
+    data = await data.json();
+    console.log(data);
+
+    trade = dqs('#vShop');
+    for(let iKey in data.categories){
+        let block = document.createElement('div');
+        let title = document.createElement('h3');
+        title.innerText = data.categories[iKey];
+        title.classList.add('text-white')
+        title.classList.add('value')
+        block.append(title);
+        trade.append(block);
+
+        let table = document.createElement('table');
+        table.classList.add('table');
+        table.classList.add('table-sm');
+        table.classList.add('table-striped');
+
+        let thead = document.createElement('thead');
+
+        let tr = document.createElement('tr');
+        tr.innerHTML = `
+        <th scope="col">#</th>
+        <th scope="col">Наименование</th>
+        <th scope="col">Цена</th>
+        `;
+
+        thead.append(tr);
+        table.append(thead);
+
+        let tbody = document.createElement('tbody');
+        tbody.id = iKey;
+        table.append(tbody);
+
+        block.append(table);
+    }
+
+    counter = 0;
+    for(let iKey in data.items){
+        let row = data.items[iKey];
+        let tr = document.createElement('tr');
+        let rowIco = '';
+        if(row.icons){
+            if(row.icons.length > 0){
+                row.icons.forEach( ic => {
+                    rowIco += `<img src="${ic}" width=16 height=16 alt='${row.name}'>`
+                })
+            }
+        }
+        tr.innerHTML = `
+        <td style="width: 100px;" class="value">${counter}</td>
+        <td class="value">${rowIco} ${row.name}</td>
+        <td style="width: 100px;" class="value text-success">${row.price}</td>
+        `
+        dqs('#' + row.category).append(tr);
+        counter++;
     }
 })
