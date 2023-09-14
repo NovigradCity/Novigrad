80654d5aacd5b70f96ad0c3386f42f7606203cae
Denis Ivanov <d.ivanov@iac.spb.ru>
2023-09-14 16:58:45 +0400

    скрины города, кэш

--- src/basic.js
+++ src/basic.js
@@ -2,21 +2,65 @@ const dqs = (selector) => {
     return document.querySelector(selector);
 }
 
+function selectCategory(event){
+    let val = event.value;
+    let items = document.tradeData.items.filter(e => e.category === val);
+
+    dqs('#tradeSelectItem').innerHTML = '';
+    items.forEach( e => {
+        dqs('#tradeSelectItem').innerHTML += `<option value='${e.name}'>${e.name}</option>`
+    });
+}
+
+function calcTrade(){
+    let amount = dqs('#tradeAmount').value;
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
     let data = await fetch('./data/catalog.json', {cache: "no-store"});
     data = await data.json();
+    document.tradeData = data;
     console.log(data);
 
     let trade = dqs('#trade');
 
     for(let iKey in data.categories){
+        let id = 'tradeTab_' + iKey;
+        let tabHead = document.createElement('li');
+        tabHead.classList.add('nav-item');
+        tabHead.setAttribute('role', 'presentation');
+        let tabButton = document.createElement('button');
+        tabButton.classList.add('nav-link');
+        tabButton.classList.add('value');
+        tabButton.classList.add('text-white');
+        tabButton.classList.add('rounded-0');
+        tabButton.id = id;
+        tabButton.setAttribute('data-bs-toggle', 'tab')
+        tabButton.setAttribute('data-bs-target', "#" + id + "Content")
+        tabButton.setAttribute('type', "button")
+        tabButton.setAttribute('role', "tab")
+        tabButton.innerText = data.categories[iKey];
+        tabHead.append(tabButton);
+        dqs('#tradeTabs').append(tabHead);
+
+        let selectOption = document.createElement('option');
+        selectOption.innerText = data.categories[iKey];
+        selectOption.value = iKey;
+        dqs('#tradeSelectCategory').append(selectOption);
+
         let block = document.createElement('div');
+        block.classList.add('tab-pane');
+        block.classList.add('fade');
+        block.id = id + "Content";
+        block.setAttribute('role', 'tabpanel')
         let title = document.createElement('h3');
         title.innerText = data.categories[iKey];
-        title.classList.add('text-white')
-        title.classList.add('value')
+        title.classList.add('text-white');
+        title.classList.add('value');
+        title.classList.add('d-none');
         block.append(title);
-        trade.append(block);
+        dqs('#tradeTabsContainer').append(block);
+        //trade.append(block);
 
         let table = document.createElement('table');
         table.classList.add('table');
@@ -50,6 +94,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
         let row = data.items[iKey];
         let tr = document.createElement('tr');
+        //tr.style.fontSize = '2em';
 
         let isSell = (row.isSell) ? 'text-success' : 'text-danger';
         //cellSell.innerHTML = "<span>Продажа: <span><span class='"+isSell+"'>" + row.price + "</span>";
@@ -59,7 +104,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
         let rowIco = '';
         if(row.icon){
-            rowIco = `<img src="${row.icon}" width=16 height=16 alt='${row.name}'>`
+            rowIco = `<img src="${row.icon}" width=24 height=24 alt='${row.name}'>`
         }
 
         tr.innerHTML = `
@@ -80,13 +125,40 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     trade = dqs('#vShop');
     for(let iKey in data.categories){
+
+        let id = 'tradevShopTab_' + iKey;
+
+        let tabHead = document.createElement('li');
+        tabHead.classList.add('nav-item');
+        tabHead.setAttribute('role', 'presentation');
+        let tabButton = document.createElement('button');
+        tabButton.classList.add('nav-link');
+        tabButton.classList.add('value');
+        tabButton.classList.add('text-white');
+        tabButton.classList.add('rounded-0');
+        tabButton.id = id;
+        tabButton.setAttribute('data-bs-toggle', 'tab')
+        tabButton.setAttribute('data-bs-target', "#" + id + "Content")
+        tabButton.setAttribute('type', "button")
+        tabButton.setAttribute('role', "tab")
+        tabButton.innerText = data.categories[iKey];
+        tabHead.append(tabButton);
+        dqs('#vShopTabs').append(tabHead);
+
         let block = document.createElement('div');
+        block.classList.add('tab-pane');
+        block.classList.add('fade');
+        block.id = id + "Content";
+        block.setAttribute('role', 'tabpanel')
         let title = document.createElement('h3');
         title.innerText = data.categories[iKey];
         title.classList.add('text-white')
         title.classList.add('value')
+
+        //tradeSelectCategory
         block.append(title);
-        trade.append(block);
+        dqs('#vShopTabsContainer').append(block)
+        //trade.append(block);
 
         let table = document.createElement('table');
         table.classList.add('table');
@@ -117,17 +189,18 @@ document.addEventListener('DOMContentLoaded', async () => {
     for(let iKey in data.items){
         let row = data.items[iKey];
         let tr = document.createElement('tr');
+        tr.style.fontSize = '1.5em';
         let rowIco = '';
         if(row.icons){
             if(row.icons.length > 0){
                 row.icons.forEach( ic => {
-                    rowIco += `<img src="${ic}" width=16 height=16 alt='${row.name}'>`
+                    rowIco += `<img src="${ic}" width=32 height=32 alt='${row.name}'>`
                 })
             }
         }
         tr.innerHTML = `
         <td style="width: 100px;" class="value">${counter}</td>
-        <td style="width: 90px;">${rowIco}</td>
+        <td style="width: 180px;">${rowIco}</td>
         <td class="value">${row.name}</td>
         <td style="width: 100px;" class="value text-success">${row.price}</td>
         `
