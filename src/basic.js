5b5865f1609a5808bbb3c6eeb8b523e1b8fcdf15
Denis Ivanov <d.ivanov@iac.spb.ru>
2023-09-27 17:53:44 +0400

    каталог

--- src/basic.js
+++ src/basic.js
@@ -2,7 +2,11 @@ const dqs = (selector) => {
     return document.querySelector(selector);
 }
 
-async function drawTables() {
+const dqsa = (selector) => {
+    return document.querySelectorAll(selector);
+}
+
+async function drawTables(sort = null) {
     let data = null;
     if (typeof document.tradeData === 'undefined') {
         data = await fetch('./data/catalog.json', {cache: "no-store"});
@@ -14,11 +18,11 @@ async function drawTables() {
             data.items.push(d2i);
         })
 
-        for(let d2c in data2.categories){
+        for (let d2c in data2.categories) {
             data.categories[d2c] = data2.categories[d2c];
         }
 
-        for(let d2l in data2.categoryPriceLogic){
+        for (let d2l in data2.categoryPriceLogic) {
             data.categoryPriceLogic[d2l] = data2.categoryPriceLogic[d2l];
         }
 
@@ -99,33 +103,33 @@ async function drawTables() {
         let isAmount = true;
         let isBuy = true;
         //console.log(logicShow);
-        if(logicShow){
-            if(typeof logicShow.showBuy !== 'undefined'){
+        if (logicShow) {
+            if (typeof logicShow.showBuy !== 'undefined') {
                 isSell = logicShow.showBuy;
             }
-            if(typeof logicShow.showAmount !== 'undefined'){
+            if (typeof logicShow.showAmount !== 'undefined') {
                 isAmount = logicShow.showAmount;
             }
-            if(typeof logicShow.showSell !== 'undefined'){
+            if (typeof logicShow.showSell !== 'undefined') {
                 isBuy = logicShow.showSell;
             }
         }
 
-        if(!isSell){
+        if (!isSell) {
             isSell = 'd-none'
-        }else{
+        } else {
             isSell = '';
         }
 
-        if(!isAmount){
+        if (!isAmount) {
             isAmount = 'd-none'
-        }else{
+        } else {
             isAmount = '';
         }
 
-        if(!isBuy){
+        if (!isBuy) {
             isBuy = 'd-none'
-        }else{
+        } else {
             isBuy = '';
         }
 
@@ -133,10 +137,10 @@ async function drawTables() {
         tr.innerHTML = `
         <th scope="col">#</th>
         <th>Иконка</th>
-        <th scope="col">Наименование</th>
-        <th scope="col" class="${isBuy}">Покупка</th>
-        <th scope="col" class="${isSell}">Продажа</th>
-        <th scope="col" class="${isAmount}">Наличие</th>
+        <th scope="col" data-table="${iKey}" data-coll="name" class="sortable">Наименование</th>
+        <th scope="col" data-table="${iKey}" data-coll="buy" class="${isBuy} sortable">Покупка</th>
+        <th scope="col" data-table="${iKey}" data-coll="sell" class="${isSell} sortable">Продажа</th>
+        <th scope="col" data-table="${iKey}" data-coll="amount" class="${isAmount} sortable">Наличие</th>
         `;
 
         thead.append(tr);
@@ -160,7 +164,7 @@ async function drawTables() {
         let rowIco = '';
         if (row.icon) {
             rowIco = `<img src="${row.icon}" width=24 height=24 alt='${row.name}'>`
-        }else if(row.icons){
+        } else if (row.icons) {
             rowIco = ``;
             row.icons.forEach(ir => {
                 rowIco += `<img src="${ir}" width=24 height=24 alt='${row.name}'>`
@@ -182,33 +186,33 @@ async function drawTables() {
         let isAmountShow = true;
         let isBuyShow = true;
 
-        if(logic){
-            if(typeof logic.showBuy !== 'undefined'){
+        if (logic) {
+            if (typeof logic.showBuy !== 'undefined') {
                 isSellShow = logic.showBuy;
             }
-            if(typeof logic.showAmount !== 'undefined'){
+            if (typeof logic.showAmount !== 'undefined') {
                 isAmountShow = logic.showAmount;
             }
-            if(typeof logic.showSell !== 'undefined'){
+            if (typeof logic.showSell !== 'undefined') {
                 isBuyShow = logic.showSell;
             }
         }
 
-        if(!isSellShow){
+        if (!isSellShow) {
             isSellShow = 'd-none'
-        }else{
+        } else {
             isSellShow = '';
         }
 
-        if(!isAmountShow){
+        if (!isAmountShow) {
             isAmountShow = 'd-none'
-        }else{
+        } else {
             isAmountShow = '';
         }
 
-        if(!isBuyShow){
+        if (!isBuyShow) {
             isBuyShow = 'd-none'
-        }else{
+        } else {
             isBuyShow = '';
         }
 
@@ -251,106 +255,132 @@ async function drawTables() {
     if (afterSelectedCategory) {
         dqs('#tradeSelectCategory').querySelector(`[value='${afterSelectedCategory}']`).setAttribute('selected', true);
     }
+
+    await prepareSort();
+}
+
+async function loadHorses() {
+    let data = [];
+    data = await fetch('./data/horses.json', {cache: "no-store"});
+    data = await data.json();
+
+    //horsesCards
+    data.horses.forEach(horse => {
+
+        let price = horse.basePrice;
+        let tmpSpeed = 0;
+        tmpSpeed = horse.speed;
+        let priceForSpeed = 0;
+        let priceForJump = 0;
+        let priceForHP = 0;
+        let tmpJump = 0;
+        if(horse.speed <= 7.5){
+            price += 1900;
+            priceForSpeed = 1900;
+        }else{
+            tmpSpeed = tmpSpeed - 7.5;
+            priceForSpeed = Math.floor((tmpSpeed / 0.5) * 1100);
+            price += priceForSpeed;
+        }
+
+        if(horse.jump <= 2.25){
+            tmpJump = horse.jump - 0.75;
+            priceForJump = Math.floor((tmpJump / 0.25) * 100);
+            price += priceForJump;
+        }else if(horse.jump > 2.25 && horse.jump < 4.75){
+            priceForJump = 600;
+            priceForJump += Math.floor((horse.jump - 2.25) / 0.25 * 200);
+            price += priceForJump;
+        }else{
+            priceForJump = 2000;
+            priceForJump += Math.floor((horse.jump - 4.75) / 0.25 * 200);
+            price += priceForJump;
+        }
+
+        if(horse.hp <= 15){
+            priceForHP = 100;
+            price += priceForHP;
+        }else if(horse.hp > 15){
+            priceForHP = data.hpMatrix[horse.hp.toString()];
+            price += priceForHP;
+        }
+        //price += horse.jump / 0.25 * 100;
+        //price +=
+
+        let html = `
+        <div class="col-xs-12 col-sm-6 col-lg-4 mb-2">
+            <div class="card">
+                <img src="${horse.img}" class="card-img-top" alt="${horse.name}">
+                <div class="card-body">
+                  <h5 class="card-title">${horse.name}</h5>
+                  <p class="card-text mb-0">
+                    ${horse.color} лошадь, находится в ${horse.stable} загон.<br>
+                    </p>
+                    <div class="row">
+                    <div class="col-1"><img width="16" src="./assets/horses/speed.png"></div>
+                    <div class="col-10">${horse.speed} (${priceForSpeed})</div>
+                    </div>
+                    
+                    <div class="row">
+                    <div class="col-1"><img width="16" src="./assets/horses/jump.png"></div>
+                    <div class="col-10">${horse.jump} (${priceForJump})</div>
+                    </div>
+                    
+                    <div class="row">
+                    <div class="col-1"><img width="16" src="./assets/horses/hp.png"></div>
+                    <div class="col-10">${horse.hp} (${priceForHP})</div>
+                    </div>
+                  
+                </div>
+                <div class="card-footer"><small class="text-body-secondary">Цена: ${price}</small></div>
+            </div>
+        </div>
+        `;
+
+        dqs('#horsesCards').innerHTML += html;
+
+    });
+}
+
+async function loadBG(){
+    fetch('./data/screens.json', {cache: "no-store"})
+        .then(data => data.json())
+        .then(data => {
+            let last = data[data.length - 1];
+            document.querySelector('body').style.backgroundImage = "url(.." + last + ")";
+            data = data.reverse();
+
+            let divScreens = document.querySelector('#cityScreens');
+            data.forEach((screen, i) => {
+                let img = document.createElement('img');
+                img.src = screen;
+                img.style.maxWidth = '100%';
+                img.style.marginBottom = '10px';
+                img.alt = 'Изображение города';
+                divScreens.append(img);
+            })
+        })
+}
+
+function sortHandler(event){
+    console.log(event);
+    console.log(this);
+}
+
+async function prepareSort(){
+    let sortColumns = dqsa('.sortable:not(.init)');
+    sortColumns.forEach( coll => {
+        coll.addEventListener('click', sortHandler.bind(coll))
+        coll.classList.add('init');
+    });
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
     document.basket = [];
     document.lastClacItem = {};
+    document.lastSort = null;
 
+    await loadBG();
     await drawTables();
-    return 0;
-
-    let data = await fetch('./data/VonderanShop.json', {cache: "no-store"});
-    data = await data.json();
-    console.log(data);
-
-    let trade = dqs('#trade');
-    let isFirst = true;
-    for (let iKey in data.categories) {
-        let id = 'tradevShopTab_' + iKey;
-        let tabHead = document.createElement('li');
-        tabHead.classList.add('nav-item');
-        tabHead.setAttribute('role', 'presentation');
-        let tabButton = document.createElement('button');
-        tabButton.classList.add('nav-link');
-        tabButton.classList.add('value');
-        tabButton.classList.add('text-white');
-        tabButton.classList.add('rounded-0');
-
-        tabButton.id = id;
-        tabButton.setAttribute('data-bs-toggle', 'tab')
-        tabButton.setAttribute('data-bs-target', "#" + id + "Content")
-        tabButton.setAttribute('type', "button")
-        tabButton.setAttribute('role', "tab")
-        tabButton.innerText = data.categories[iKey];
-        tabHead.append(tabButton);
-        dqs('#tradeTabs').append(tabHead);
-
-        let block = document.createElement('div');
-        block.classList.add('tab-pane');
-        block.classList.add('fade');
-        if (isFirst) {
-            //block.classList.add('active');
-            //block.classList.add('show');
-            isFirst = false;
-        }
-        block.id = id + "Content";
-        block.setAttribute('role', 'tabpanel')
-        let title = document.createElement('h3');
-        title.innerText = data.categories[iKey];
-        title.classList.add('text-white')
-        title.classList.add('value')
-
-        //tradeSelectCategory
-        block.append(title);
-        dqs('#tradeTabsContainer').append(block)
-        //trade.append(block);
-
-        let table = document.createElement('table');
-        table.classList.add('table');
-        table.classList.add('table-sm');
-        table.classList.add('table-striped');
-
-        let thead = document.createElement('thead');
-
-        let tr = document.createElement('tr');
-        tr.innerHTML = `
-        <th scope="col">#</th>
-        <th>Иконка</th>
-        <th scope="col">Наименование</th>
-        <th scope="col">Цена</th>
-        `;
-
-        thead.append(tr);
-        table.append(thead);
-
-        let tbody = document.createElement('tbody');
-        tbody.id = iKey;
-        table.append(tbody);
-
-        block.append(table);
-    }
-
-    counter = 0;
-    for (let iKey in data.items) {
-        let row = data.items[iKey];
-        let tr = document.createElement('tr');
-        tr.style.fontSize = '1.5em';
-        let rowIco = '';
-        if (row.icons) {
-            if (row.icons.length > 0) {
-                row.icons.forEach(ic => {
-                    rowIco += `<img src="${ic}" width=32 height=32 alt='${row.name}'>`
-                })
-            }
-        }
-        tr.innerHTML = `
-        <td style="width: 100px;" class="value">${counter}</td>
-        <td style="width: 180px;">${rowIco}</td>
-        <td class="value">${row.name}</td>
-        <td style="width: 100px;" class="value text-success">${row.price}</td>
-        `
-        dqs('#' + row.category).append(tr);
-        counter++;
-    }
+    await loadHorses();
 })
